21b73f73371aad9ba34cfbe246231c8722a7df9e
ponCHANNN <mizuki.7302@gmail.com>
2022-09-07 13:24:59 +0900

    add bot msg (WIP)

--- shift.js
+++ shift.js
@@ -1,11 +1,22 @@
 const discord = require("discord.js");
 const client = new discord.Client();
+const disc = require("./disc.json");
 //npm install node-google-calendar
 
 module.exports = {
     create : this.create
 }
 
-function create () {
+let shiftMsgEmbed = new MessageEmbed();
+shiftMsgEmbed.setTitle(disc.shiftMsgEmbed.Title);
+shiftMsgEmbed.setDiscription(disc.shiftMsgEmbed.Discription);
 
+function create (msg) {
+    let sentMsg = msg.channel.send(shiftMsgEmbed);
+
+    for (let i = 1; i <= 4; i++) {
+        sentMsg.react(i);
+    }
+
+    sentMsg.awaitReactions()
 }
