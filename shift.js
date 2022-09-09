68a82119cd101bcf1abe3895d01b3a39e668d0eb
ponCHANNN <mizuki.7302@gmail.com>
2022-09-09 09:31:29 +0900

    add count system (WIP)

--- shift.js
+++ shift.js
@@ -14,16 +14,32 @@ let shiftMsgEmbed = new discord.MessageEmbed()
 function create (msg) {
     msg.channel.send(shiftMsgEmbed)
     .then(sentMsg => {
-        for (let i = 1; i <= 4; i++) {
+        for (let i = 1; i <= 5; i++) {
             setTimeout(() => {sentMsg.react(disc.emoji[i]);}, 100);
         }
 
-        sentMsg.awaitReactions(r => r.emoji.name == disc.emoji[4], {max: 2})
-    .then(collected => 
-        console.log(collected.first().message.reactions.cache.get(disc.emoji[1]).count));
+        sentMsg.awaitReactions(r => r.emoji.name == disc.emoji[5], {max: 2})
+    .then(collected => {
+        let one = collected.first().message.reactions.cache.get(disc.emoji[1]).count;
+        let two = collected.first().message.reactions.cache.get(disc.emoji[2]).count;
+        let three = collected.first().message.reactions.cache.get(disc.emoji[3]).count;
+        let four = collected.first().message.reactions.cache.get(disc.emoji[4]).count;
+        inputContent(msg, one, two, three, four);
+    });
         
     })
 
     
+    
+}
+
+function inputContent (msg, one, two, three, four) {
+    let timeCount = {one, two, three, four};
+
+    for (let i in timeCount) {
+        if (timeCount[i] !== 2) continue;
+        let min = timeCount[i];
+        break;
+    }
     
 }
